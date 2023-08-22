934dc2f9699396c597606f6f51fa401bc6fdf631
ShiriForer <shiri.forer@gmail.com>
2023-08-22 10:23:40 +0300

    changes to capture_screenshot

--- BasePage.py
+++ BasePage.py
@@ -1,34 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    def capture_screenshot(self, filename):
-        screenshot_filename = f"{filename}.png"
-        self.driver.save_screenshot(screenshot_filename)
-        print(f"Screenshot saved: {screenshot_filename}")
+    def capture_screenshot(self):
+        screenshot = screenshot.png
+        self.driver.save_screenshot(screenshot)
+        print("The element was not found. A screenshot was saved")
 
     def click_element(self, locator_type, locator_value):
-        self.driver.find_element(locator_type, locator_value).click()
+        try:
+            self.driver.find_element(locator_type, locator_value).click()
+        except NoSuchElementException:
+            capture_screenshot()
 
     def enter_text(self, locator_type, locator_value, text):
-        self.driver.find_element(locator_type, locator_value).send_keys(text)
+        try:
+            self.driver.find_element(locator_type, locator_value).send_keys(text)
+        except NoSuchElementException:
+            capture_screenshot()
 
     def select_option_by_value(self, locator_type, locator_value, option_value):
-        #click to open the drop-down list
-        multiple_choice_field = self.driver.find_element(locator_type, locator_value).click()
+        #click to open a drop-down list
+        try:
+            multiple_choice_field = self.driver.find_element(locator_type, locator_value).click()
+        except NoSuchElementException:
+            capture_screenshot()
         # select an option from the list
         choose_option = Select(multiple_choice_field)
         choose_option.select_by_value(option_value)
 
     def select_option_by_visible_text(self, locator_type, locator_value, visible_text):
         # click to open the drop-down list
-        multiple_choice_field = self.driver.find_element(locator_type, locator_value).click()
+        try:
+            multiple_choice_field = self.driver.find_element(locator_type, locator_value).click()
+        except NoSuchElementException:
+            capture_screenshot()
         # select an option from the list
         choose_option = Select(multiple_choice_field)
         choose_option.select_by_visible_text(visible_text)
--- BuyMeTests.py
+++ BuyMeTests.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
 from BuyMe import BuyMeIntroPage, RegistrationForm, HomeScreen
 
 class test_Buyme(TestCase):
@@ -14,25 +12,12 @@ class test_Buyme(TestCase):
         # page load timeout
         self.driver.set_page_load_timeout(30)
 
-    # add here explicit waits? And for which fields?
-    # def test_explicit_wait(self):
-    #     # explicit wait
-    #     element = WebDriverWait(self.driver, 10).until(
-    #         expected_conditions.element_to_be_clickable((By.CLASS_NAME, "er8xn")))
-    #     element.send_keys('helo')
-
-    def tearDown(self):
-        self.driver.quit()
-
-class test_BuyMeIntroPage(TestCase):
     def test_intro_page(self):
         self.intro_page = BuyMeIntroPage(self.driver)
         self.intro_page.close_intro_banner()
         self.intro_page.click_to_register()
         self.intro_page.register()
 
-
-class test_RegistrationForm(TestCase):
     def test_registration_form(self):
         self.registration_form = RegistrationForm(self.driver)
         self.registration_form.fill_in_first_name()
@@ -42,10 +27,13 @@ class test_RegistrationForm(TestCase):
         self.registration_form.agree_to_terms()
         self.registration_form.submit_registration_form()
 
-class test_HomeScreen(TestCase):
     def test_home_screen(self):
         self.home_screen = HomeScreen(self.driver)
         self.home_screen.pick_price_point()
         self.home_screen.pick_region()
         self.home_screen.pick_category()
         self.home_screen.find_gift()
+
+    def tearDown(self):
+        self.driver.quit()
+
